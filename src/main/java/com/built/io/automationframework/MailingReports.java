4f54f1d9f1e699c442c95949dd799c2943998617
Mahesh Chilumula <mahesh.chilumula@raweng.com>
2016-08-31 10:45:43 +0530

    remove

--- src/main/java/com/built/io/automationframework/MailingReports.java
+++ src/main/java/com/built/io/automationframework/MailingReports.java
@@ -7,8 +7,8 @@ import javax.mail.internet.*;
 
 public class MailingReports {
 
-    static final String FROM = "mahesh.chilumula@raweng.com";   
-    static final String TO = "contentstack-dev@raweng.com";                                                   
+    static final String FROM = "mahesh.chilumula@gmail.com";   
+    static final String TO = "example@gmail.com";                                                   
     static final String BODY = "Below are the links to automation reports generated for UI and API, respectively :"+ '\n' + '\n' + "UI: http://uiautomation.builtapp.io/assets/"+Reports.report_name +'\n' + '\n'+ "API: http://uiautomation.builtapp.io/assets/"+"test.jtl-report.html"+'\n'+'\n' + "------" +'\n' +"Regards,"+ '\n' + "QA Team";
     static final String SUBJECT = "Contentstack Development UI Report | " + BasePageActions.report_date;
     static final String SMTP_USERNAME = "AKIAI56YLBVETIWY55NA"; 
